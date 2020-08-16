beb1d09abf94c5e3978785bf3fc1c55885927224
Sedki Sghairi <sedkisghairi017@gmail.com>
2020-08-16 19:49:47 +0400

    changed toggle with an if statement to see if it works better in mac

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ const navToggle = document.getElementById('nav-toggle');
 const links = document.getElementById('links');
 
 navToggle.addEventListener('click', function() {
-	links.classList.toggle('show-links');
+	// links.classList.toggle('show-links');
+	if (links.classList.contains('show-links')) {
+		links.classList.remove('show-links');
+	} else {
+		links.classList.add('show-links');
+	}
 });
